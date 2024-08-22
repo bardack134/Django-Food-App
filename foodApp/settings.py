28a4698e185dfbbf453c8b43cada18057f927af7
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-08-22 16:37:52 +0900

    settings

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -29,15 +29,10 @@ TWILIO_AUTH_TOKEN=env("TWILIO_AUTH_TOKEN")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-w*4oai7d#v22_9lgk3dahs$_=2fh3r^#2lnqekh*!2^*7efu%$')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
